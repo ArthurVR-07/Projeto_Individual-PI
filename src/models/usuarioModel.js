f6e9d3ff0ac20231180dfdf0c23e40511e7153f2
ArthurVR-07 <arthur.vrocha@sptech.school>
2026-04-27 21:04:32 -0300

    Feat: Início desenvolvimento quiz

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -13,6 +13,13 @@ function autenticar(email, senha) {
 function cadastrar(nome, username, email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, username);
     
+    var instrucaoSql_select_email = `
+        SELECT email FROM usuario WHERE email = '${email}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql_select_email);
+
+
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
