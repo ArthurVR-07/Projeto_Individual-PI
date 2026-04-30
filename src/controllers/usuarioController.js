cb9987fd439f93e522b044db977c7af6fb11a1e5
ArthurVR-07 <arthur.vrocha@sptech.school>
2026-04-30 13:13:00 -0300

    Feat: Criptografia de senha com for/vetor

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,6 +1,16 @@
 var usuarioModel = require("../models/usuarioModel");
 // var aquarioModel = require("../models/aquarioModel");
 
+// Definindo a tabela ASCII estendida com os principais caracteres do teclado ABNT para criptografar / descriptografar a senha
+let tabelaASCII = {
+    ' ': 32,  '!': 33,  '"': 34,  '#': 35,  '$': 36,  '%': 37,  '&': 38,  "'": 39, '(': 40,  ')': 41,  '*': 42,  '+': 43,  ',': 44,  '-': 45,  '.': 46,  '/': 47, '0': 48,  '1': 49,  '2': 50,  '3': 51,  '4': 52,  '5': 53,  '6': 54,  '7': 55, '8': 56,  '9': 57,  ':': 58,  ';': 59,  '<': 60,  '=': 61,  '>': 62,  '?': 63,
+    '@': 64,  'A': 65,  'B': 66,  'C': 67,  'D': 68,  'E': 69,  'F': 70,  'G': 71, 'H': 72,  'I': 73,  'J': 74,  'K': 75,  'L': 76,  'M': 77,  'N': 78,  'O': 79, 'P': 80,  'Q': 81,  'R': 82,  'S': 83,  'T': 84,  'U': 85,  'V': 86,  'W': 87, 'X': 88,  'Y': 89,  'Z': 90,  '[': 91,  '\\': 92, ']': 93,  '^': 94,  '_': 95,
+    '`': 96,  'a': 97,  'b': 98,  'c': 99,  'd': 100, 'e': 101, 'f': 102, 'g': 103, 'h': 104, 'i': 105, 'j': 106, 'k': 107, 'l': 108, 'm': 109, 'n': 110, 'o': 111, 'p': 112, 'q': 113, 'r': 114, 's': 115, 't': 116, 'u': 117, 'v': 118, 'w': 119, 'x': 120, 'y': 121, 'z': 122, '{': 123, '|': 124, '}': 125, '~': 126, 'Ç': 128, 
+    'é': 130, 'â': 131, 'à': 133, 'ç': 135, 'ê': 136, 'É': 144, 'ô': 147, '¢': 155, '£': 156, '¥': 157, '×': 158, 'á': 160, 'í': 161, 'ó': 162, 'ú': 163, 'ª': 166, 'º': 167, '¿': 168, '¬': 170, '½': 171, '¼': 172, '¡': 173, '«': 174, '»': 175, 'Á': 181, 'Â': 182, 'À': 183, 'ã': 198, 'Ã': 199, 'Ê': 210, 'Í': 214, 'Ó': 224, 
+    'Ô': 226, 'õ': 228, 'Õ': 229, 'Ú': 233, '±': 241, '¾': 243, '§': 245, '÷': 246, 
+    '°': 248, '¹': 251, '³': 252, '²': 253
+};
+
 function autenticar(req, res) {
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
@@ -11,30 +21,28 @@ function autenticar(req, res) {
         res.status(400).send("Sua senha está indefinida!");
     } else {
 
-        usuarioModel.autenticar(email, senha)
+        // Criptografando a senha denovo para coincidir com a senha no banco de dados
+        let vetor_separar_senha = [];
+
+        // Guarda o decimal de cada caractere no vetor
+        for (let i = 0; i < senha.length; i++) {
+            vetor_separar_senha[i] = (tabelaASCII[senha[i]] + 3) * 2;
+        }
+
+        // Transforma o vetor em string para postar no banco de dados
+        let senhaCriptografada = "";
+        for (let i = 0; i < vetor_separar_senha.length; i++) {
+            senhaCriptografada += vetor_separar_senha[i];
+            if (i < vetor_separar_senha.length - 1) {
+                senhaCriptografada += "-";
+            }
+        }
+
+        usuarioModel.autenticar(email, senhaCriptografada)
             .then(
                 function (resultadoAutenticar) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
-
-                    /* if (resultadoAutenticar.length == 1) {
-                        console.log(resultadoAutenticar);
-
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
-                    } */
 
                     if (resultadoAutenticar.length == 1) {
                     res.json({
@@ -83,8 +91,25 @@ function cadastrar(req, res) {
         res.status(400).send("Sua empresa a vincular está undefined!");
     }*/ else {
 
+        // Criptografando a senha
+        let vetor_separar_senha = [];
+
+        // Guarda o decimal de cada caractere no vetor
+        for (let i = 0; i < senha.length; i++) {
+            vetor_separar_senha[i] = (tabelaASCII[senha[i]] + 3) * 2;
+        }
+
+        // Transforma o vetor em string para postar no banco de dados
+        let senhaCriptografada = "";
+        for (let i = 0; i < vetor_separar_senha.length; i++) {
+            senhaCriptografada += vetor_separar_senha[i];
+            if (i < vetor_separar_senha.length - 1) {
+                senhaCriptografada += "-";
+            }
+        }
+
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, username)
+        usuarioModel.cadastrar(nome, username, email, senhaCriptografada)
             .then(
                 function (resultado) {
                     res.json(resultado);
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -11,13 +11,7 @@ function autenticar(email, senha) {
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
 function cadastrar(nome, username, email, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, username);
-    
-    var instrucaoSql_select_email = `
-        SELECT email FROM usuario WHERE email = '${email}';
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql_select_email);
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, username, email, senha);
 
 
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
